6727a8c6563e164962a0c3873b30264d92bc6012
rejanesilva88855sg-dot <rejanesilva88855sg@gmail.com>
2025-12-06 21:55:37 -0300

    background.js

--- background.js
+++ background.js
@@ -1,16 +1,17 @@
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    if (msg.action === "getCards") {
-        chrome.storage.local.get(["cards"], res => {
-            sendResponse(res.cards || []);
-        });
-        return true;
-    }
+    if (msg.action === "obterCartaoSelecionado") {
+        chrome.storage.local.get(["cartoes", "cartaoSelecionado"], (data) => {
+            const cartoes = data.cartoes || [];
+            const index = data.cartaoSelecionado;
 
-    if (msg.action === "fillCard") {
-        chrome.storage.local.get(["cards"], res => {
-            let card = res.cards.find(c => c.id === msg.id);
-            sendResponse(card);
+            if (index === undefined || !cartoes[index]) {
+                sendResponse(null);
+                return;
+            }
+
+            sendResponse(cartoes[index]);
         });
+
         return true;
     }
 });
